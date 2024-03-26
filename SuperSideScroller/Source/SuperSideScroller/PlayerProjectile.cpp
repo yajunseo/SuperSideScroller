12f224dcab064906edd0eaa2abdab987ad4b6dcd
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2024-03-26 22:19:56 +0900

    [닷지볼/AI/야준서] 투사체 적용 중

--- SuperSideScroller/Source/SuperSideScroller/PlayerProjectile.cpp
+++ SuperSideScroller/Source/SuperSideScroller/PlayerProjectile.cpp
@@ -3,15 +3,34 @@
 
 #include "PlayerProjectile.h"
 
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
 // Sets default values
 APlayerProjectile::APlayerProjectile()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	CollisionComp->InitSphereRadius(15.0f);
+	CollisionComp->BodyInstance.SetCollisionProfileName("BlockAll");
+	CollisionComp->OnComponentHit.AddDynamic(this, &APlayerProjectile::OnHit);
 
+	RootComponent = CollisionComp;
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
+	ProjectileMovement->UpdatedComponent = CollisionComp;
+	ProjectileMovement->ProjectileGravityScale = 0.0f;
+	ProjectileMovement->InitialSpeed = 800.0f;
+	ProjectileMovement->MaxSpeed = 800.0f;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepWorldTransform);
+
+	InitialLifeSpan = 3.0f;
+	
 }
 
 void APlayerProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
 	FVector NormalImpulse, const FHitResult& Hit)
 {
+	UE_LOG(LogTemp, Warning, TEXT("Hit"));
 }
